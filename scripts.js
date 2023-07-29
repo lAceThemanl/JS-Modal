6ad728554d081850186abccba788f0d884b1aee9
acetheman <nolasco.hermen@gmail.com>
2023-07-29 08:53:22 -0500

    added waldo found modal

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@
 
 let openBtn = document.getElementById('help-btn');
 let modalContainer = document.getElementById('modal-help');
+let header = document.querySelector('.header');
+let openBtn2 = document.getElementById('modal-btn');
+let modalContainer2 = document.getElementById('modal-help-2');
 let closeBtn = document.getElementById('close-btn');
 
 // Event Listeners
@@ -9,11 +12,22 @@ let closeBtn = document.getElementById('close-btn');
 openBtn.addEventListener('click', function() {
 
     modalContainer.style.display = 'block';
+    header.style.display = 'none';
+    openBtn.style.display = 'none';
+
+});
+
+openBtn2.addEventListener('click', function() {
+
+    modalContainer2.style.display = 'block';
+    // openBtn2.style.display = 'none';
+
 });
 
 closeBtn.addEventListener('click', function() {
 
     modalContainer.style.display = 'none';
+
 });
 
 window.addEventListener('click', function(e) {
