43b56c70534aba176b12c2bf065d9f34ffe6725c
acetheman <nolasco.hermen@gmail.com>
2023-07-30 10:43:09 -0500

    added Waldo Found modals

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
 // Variables
 
+let header = document.querySelector('.header');
 let openBtn = document.getElementById('help-btn');
 let modalContainer = document.getElementById('modal-help');
-let header = document.querySelector('.header');
+
 let openBtn2 = document.getElementById('modal-btn');
 let modalContainer2 = document.getElementById('modal-help-2');
-let closeBtn = document.getElementById('close-btn');
+
+let btnWaldoFound = document.querySelector('.btn-waldo-found');
+let btnWaldoFoundClicked = document.querySelector('.btn-waldo-found-clicked');
+let modalWaldoFound = document.getElementById('modal-waldo-found');
+let btnReset = document.getElementById('btn-reset');
 
 // Event Listeners
 
@@ -20,19 +25,27 @@ openBtn.addEventListener('click', function() {
 openBtn2.addEventListener('click', function() {
 
     modalContainer2.style.display = 'block';
-    // openBtn2.style.display = 'none';
 
 });
 
-closeBtn.addEventListener('click', function() {
+btnWaldoFound.addEventListener('click', function() {
 
+    modalWaldoFound.style.display = 'block';
+    btnWaldoFoundClicked.style.display = 'block';
     modalContainer.style.display = 'none';
+    header.style.display = 'none';
+    openBtn.style.display = 'none';
+    modalContainer2.style.display = 'none';
 
 });
 
-window.addEventListener('click', function(e) {
+btnReset.addEventListener('click', function() {
 
-    if (e.target === modalContainer) {
-        modalContainer.style.display = 'none';
-    }
+    modalWaldoFound.style.display = 'none';
+    btnWaldoFoundClicked.style.display = 'none';
+    modalContainer.style.display = 'none';
+    header.style.display = 'block';
+    openBtn.style.display = 'block';
+    modalContainer2.style.display = 'none';
+
 });
